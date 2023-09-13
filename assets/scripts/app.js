edef9c19ea34b62ffab521855932eadbd2f1cea1
leobaksaj <baksajleo22@gmail.com>
2023-09-13 21:15:57 +0200

    second commit - variables

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,25 @@
-alert('This works!');
+//  let userName = 'Max'; best practice camelCase
+//  const totalUsers = 15; //  You can't change a value for const
+ 
+//VARIABLES
+// Allowed variables
+// let userName5 
+// let $userNAme
+// let _userName
+
+// Not allowed variables
+// let user_name
+// let 5userName
+// let user-name
+// let let
+
+//OPERATORS
+// + - * / % ** =
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+currentResult = (currentResult + 10) * 3 / 2 - 1;
+
+let calculationDescription = '(0 + 10) * 3 / 2 - 1';
+
+outputResult( currentResult, calculationDescription );
