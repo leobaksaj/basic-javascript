aedaa0bba682b5bd63ac3a7b1b92218574067f65
leobaksaj <baksajleo22@gmail.com>
2023-10-04 21:38:34 +0200

    third commit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -21,7 +21,8 @@
 
 const defaultResult = 0;
 let currentResult   = defaultResult;
-
+let logEntries =[];
+ 
 function getUserNumberInput(){
     return parseInt( userInput.value );
 }
@@ -32,11 +33,25 @@ function createAndWriteOutput( operator, resultBeforeCalc, calcNumber  ){
 
 }
 
+function writeToLog( operationIdentifier, prevResult, operationNumber, newResult ){
+    const logEntry = {
+        operation:  operationIdentifier,
+        prevResult: prevResult,
+        number:     operationNumber,
+        result:     newResult,
+    };
+    logEntries.push( logEntry );
+    console.log( logEntry.operation );
+    console.log( logEntries );
+
+}
+
 function add(){
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
     currentResult       = currentResult + enteredNumber;
     createAndWriteOutput( '+', initialResult , enteredNumber  );
+    writeToLog( 'ADD', initialResult, enteredNumber, currentResult );
 }
 
 function subtract(){
@@ -44,6 +59,7 @@ function subtract(){
     const initialResult = currentResult;
     currentResult       = currentResult - enteredNumber;
     createAndWriteOutput( '-', initialResult , enteredNumber  );
+    writeToLog( 'SUBTRACT', initialResult, enteredNumber, currentResult );
 }
 
 function multiply(){
@@ -51,6 +67,7 @@ function multiply(){
     const initialResult = currentResult;
     currentResult       = currentResult * enteredNumber;
     createAndWriteOutput( '*', initialResult , enteredNumber  );
+    writeToLog( 'MULTIPLY', initialResult, enteredNumber, currentResult );
 
 }
 
@@ -59,6 +76,7 @@ function divide(){
     const initialResult = currentResult;
     currentResult       = currentResult / enteredNumber;
     createAndWriteOutput( '/', initialResult , enteredNumber  );
+    writeToLog( 'DIVIDE', initialResult, enteredNumber, currentResult );
 }
 
 addBtn.addEventListener( 'click', add );
